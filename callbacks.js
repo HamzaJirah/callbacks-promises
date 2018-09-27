82bbad6cc255e7a78cfe663e41b6af65cd0793e8
HamzaJirah <hamzajirah@gmail.com>
2018-09-27 19:56:23 +0100

    created array of objects

--- callbacks.js
+++ callbacks.js
@@ -1,2 +1,12 @@
 //create an array of objects
 
+const blogPost = [
+  {
+    title: "Post One",
+    body: "This is post one"
+  },
+  {
+    title: "Post Two",
+    body: "This is post two"
+  }
+]

4cefb1776e6ea4dd5f5a7582dc18743eba140aa4
HamzaJirah <hamzajirah@gmail.com>
2018-09-28 05:07:57 +0100

    added getPost and createPost functions

---
+++ promises.js
@@ -0,0 +1,29 @@
+//create an array of objects
+
+const blogPost = [
+  {
+    title: "Post One",
+    body: "This is post one"
+  },
+  {
+    title: "Post Two",
+    body: "This is post two"
+  }
+];
+
+function getPost(){
+  setTimeout(()=>{
+    let output = '';
+    blogPost.forEach((post, index)=>{
+      output += `<li>${post.title}</li>`;
+    })
+    document.body.innerHTML = output;
+  }, 1000)
+}
+
+function createPost (post, callback){
+  setTimeout(()=>{
+    blogPost.push(post);
+    callback();
+  }, 2000);
+}
